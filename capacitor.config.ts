4289e25c260c205bec71f5955c9679238606eb56
Adrian Vargas <adrianfelipevr@gmail.com>
2025-02-25 19:00:15 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appToDo',
+  webDir: 'www'
+};
+
+export default config;
