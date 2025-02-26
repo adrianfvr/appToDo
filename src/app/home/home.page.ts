bff1ccf2fb5aae5758f58826c71c98b30f532a3c
Adrian Vargas <adrianfelipevr@gmail.com>
2025-02-26 13:37:24 -0500

    Test App To Do

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ITask } from '../interfaces/ITask';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,43 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  tasks: ITask[] = [];
 
-  constructor() {}
+  constructor(private _AlertController: AlertController) {}
 
+  async open() {
+    const alert = await this._AlertController.create({
+      header: 'Agregar nueva tarea',
+      inputs: [
+        {
+          type: 'text',
+          name: 'title',
+          placeholder: 'Ingrese titulo',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Agregar',
+          handler: (data) => {
+            this.tasks.push({
+              id: this.tasks.length + 1,
+              title: data.title,
+              done: false,
+            });
+          },
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  toggleTaskDone = (task: ITask) => {
+    task.done = !task.done;
+    console.log(this.tasks);
+  };
 }
